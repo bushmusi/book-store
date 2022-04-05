216ef65a08d6729e80ea2347b4b240c71b769838
Bushra Mustofa <bushera.mestofa@gmail.com>
2022-04-05 22:36:25 +0300

    Add configure store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { combineReducers, createStore } from 'react-redux';
+import bookReducre from './books/books';
+import catagoryReducer from './catagories/catagories'
+
+const rootReducer = combineReducers({
+    reducer: [bookReducre, catagoryReducer]
+})
+
+const store = createStore(rootReducer);
+
+store.subcribe(() => console.log(store.getState()))
+
+
